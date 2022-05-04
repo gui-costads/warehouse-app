23b56d0f791e4ed267586cedf70a875df416a48a
gui-costads <guilhermecosta.ds1@gmail.com>
2022-05-04 14:33:25 -0300

    Adiciona funcionalidade de cadastrar novos galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,5 +1,17 @@
 class WarehousesController < ApplicationController
     def show
-    @warehouse = Warehouse.find(params[:id])
+    	@warehouse = Warehouse.find(params[:id])
     end
+
+    def new
+			@warehouse = Warehouse.new
+    end
+
+		def create 
+			warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :area, :address, :cep, :description)
+			new_warehouse = Warehouse.new(warehouse_params)
+			new_warehouse.save()
+
+			redirect_to root_path
+		end
 end
---
+++ spec/system/user_register_new_warehouse_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra novo galpão' do
+  it 'a partir da página inicial' do
+		#Arrange
+
+		#Act
+		visit root_path
+		click_on 'Cadastrar Galpão'
+
+		#Assert
+		expect(page).to have_content('Nome')
+		expect(page).to have_content('Código')
+		expect(page).to have_content('Cidade')
+		expect(page).to have_content('Área')
+		expect(page).to have_content('Endereço')
+		expect(page).to have_content('Cep')
+		expect(page).to have_content('Descrição')
+	end  
+	it 'com sucesso' do
+		#Arrange
+
+		#Act
+		visit root_path
+		click_on 'Cadastrar Galpão'
+		fill_in 'Nome', with: 'Brasília'
+		fill_in 'Código', with: 'BSB'
+		fill_in 'Cidade', with: 'Brasília'
+		fill_in 'Área', with: '30000'
+		fill_in 'Endereço', with: 'Av. principal, numero 5'
+		fill_in 'Cep', with: '38000-000'
+		fill_in 'Descrição', with: 'Galpão do distrito federal'
+		click_on 'Salvar'
+		
+		#Assert
+		expect(current_path).to eq root_path
+		expect(page).to have_content('Brasília')
+		expect(page).to have_content('BSB')
+		expect(page).to have_content('Brasília')
+		expect(page).to have_content('30000')
+	end
+end
